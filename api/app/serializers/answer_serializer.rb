47fdb33bae740e023bf83b3c0f8c6a4cfb5aa9f3
¨kericho¨ <lvisrono90@gmail.com>
2023-04-18 08:18:00 +0300

    made changes on ci

--- api/app/serializers/answer_serializer.rb
+++ api/app/serializers/answer_serializer.rb
@@ -1,3 +1,3 @@
 class AnswerSerializer < ActiveModel::Serializer
-  attributes :id, :content, :question_id, :assessment_id, :interviewee_id, :created_at, :updated_at
+  attributes :id, :content, :question_id, :assessment_id, :interviewee_id, :created_at, :updated_at 
 end
