0b4a29de8c3f2daaf5cc59eea99181f7b44c0dbe
grace_aloo <graceaaloo@gmail.com>
2023-04-24 00:37:51 +0300

    fixing merging errors

--- api/app/serializers/question_serializer.rb
+++ api/app/serializers/question_serializer.rb
@@ -1,8 +1,3 @@
 class QuestionSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
   attributes :id, :content, :question, :feedback, :assessment_id, :created_at, :updated_at
-=======
-  attributes :id, :question, :content, :answers, :feedback, :assessment_id, :created_at, :updated_at
-  has_many :answers
->>>>>>> d5cab90122950c5f6c8e24951b2644b2a3b32c5f
 end
--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -9,8 +9,8 @@ const LandingPage = () => {
         <img src="https://media.istockphoto.com/id/1391105410/photo/meeting-one-on-one-female-therapist-gestures-toward-unrecognizable-woman.jpg?b=1&s=170667a&w=0&k=20&c=VM4tcYNYwMano7e55Yn-9hmIfAROXMbvrfXDuGMi0C8=" alt="About Us" className="about-image" />
         <div className="about-text">
           <h2>Smart Recruiters </h2>
-          <h2>For Job</h2>
-          <h2>Seekers</h2>
+          <h2>For Job Seekers</h2>
+          {/* <h2>Seekers</h2> */}
           <p className='paragh'>our mission is to connect people with jobs at scale. In</p>
           <p className='paragh'>order for businesses to hire the best talent,people</p>
           <p className='paragh'>deserve to easily find a job they love</p>
