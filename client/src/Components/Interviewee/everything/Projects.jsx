363c67034e6ea2d19a161cacf5a7e3538eb05327
¨kericho¨ <lvisrono90@gmail.com>
2023-04-24 14:22:57 +0300

    update client

--- client/src/Components/Interviewee/everything/Projects.jsx
+++ client/src/Components/Interviewee/everything/Projects.jsx
@@ -67,6 +67,7 @@ const Title = styled.h3`
   font-weight: 500;
   @media screen and (min-width: 320px) and (max-width: 1080px) {
     font-size: 1rem;
+    cursor: pointer;
   }
 `;
 const SubTitle = styled.h5`
