1998e6ef5aeb0662a6de92779631a33b6ceaaf06
grace_aloo <graceaaloo@gmail.com>
2023-04-27 13:21:22 +0300

    assessments list error fixing

--- api/app/serializers/answer_serializer.rb
+++ api/app/serializers/answer_serializer.rb
@@ -1,6 +1,5 @@
 class AnswerSerializer < ActiveModel::Serializer
   attributes :id,:interviewee_id,:grades, :content, :feedback, :assessment_id, :created_at, :updated_at
-  beolongs_to :code_challenge
+  belongs_to :code_challenge
  
-  end
 end
