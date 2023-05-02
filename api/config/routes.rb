9b8a5b9a7124e7d5cc89a4cb64537600d8f9fc83
grace_aloo <graceaaloo@gmail.com>
2023-05-02 06:19:34 +0300

    error handling

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   post '/interviewee/reset', to:"interviewees#reset_password"
   get '/interviewee/:id', to:"interviewees#show"
   get '/interviewee/grades/:id', to: "interviewees#show_grades"
-  get '/interviewees/completed', to: "interviewees#ccompleted_assessments"
+  get '/interviewees/completed', to: "interviewees#completed_assessments"
   #to delete a user 
  delete '/interviewee/delete/:id', to:"interviewees#delete_account" 
 
